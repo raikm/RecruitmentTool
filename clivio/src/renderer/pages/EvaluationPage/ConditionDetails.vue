d4f06262f1d40e5bb68aede2e6176b2e481647e3
raikm <raik.mueller@copra-system.de>
2020-08-13 11:59:27 +0200

    Detail Popup prototype

---
+++ clivio/src/renderer/pages/EvaluationPage/ConditionDetails.vue
@@ -0,0 +1,94 @@
+<template>
+  <div>
+    <div
+      class="type-tag"
+      :class="{
+        greenBackgroundClass: currentCriterium.criterium_type == 'EK',
+        redBackgroundClass: currentCriterium.criterium_type == 'AK',
+      }"
+    >
+      {{ currentCriterium.criterium_type }}
+    </div>
+    <h1 id="condition-name">{{ currentCondition.name }}</h1>
+    <h2 class="matches-subtitle">Matches</h2>
+
+    <div
+      class="hit-container"
+      v-for="result in currentCondition.evaluation_results.hits"
+      :key="result.cda_id"
+      @click="openCDA(result.cda_id)"
+    >
+      <div class="cda-datum">12.05.2020 (3d)</div>
+      
+
+      <ul>
+      <li class="hit-result" v-for="hit in result.hit_result" :key="hit">
+        {{ hit }}
+      </li>
+
+      </ul>
+    </div>
+    <h2 class="matches-subtitle">Negative Matches</h2>
+
+    <div
+      class="hit-container"
+      v-for="result in currentCondition.evaluation_results.hits"
+      :key="result.cda_id"
+      @click="openCDA(result.cda_id)"
+    >
+      <div class="cda-datum">12.05.2020 (3d)</div>
+      
+
+      <ul>
+      <li class="hit-result" v-for="hit in result.hit_result" :key="hit">
+        {{ hit }}
+      </li>
+
+      </ul>
+    </div>
+
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ConditionDetails",
+  components: {},
+  props: ["currentCondition", "currentCriterium"],
+  methods: {
+    openCDA(cda_id) {
+      console.log("open" + cda_id);
+    },
+  },
+};
+</script>
+
+<style>
+.matches-subtitle{
+    margin-left: 5%;
+}
+
+
+.hit-container {
+  width: 90%;
+  margin: 0 5%;
+  padding: 1%;
+  border-width: 0.5px;
+  border-color: teal;
+  border-style: solid;
+  display: inline-block;
+}
+
+
+
+.cda-datum {
+  float: right;
+  
+}
+.hit-result{
+    
+}
+
+
+</style>
--- clivio/src/renderer/pages/EvaluationPage/ResultList.vue
+++ clivio/src/renderer/pages/EvaluationPage/ResultList.vue
@@ -8,7 +8,7 @@
         hoverable
         :show-detail-icon="false"
         custom-detail-row
-        detail-key="name"
+        detail-key="patient_id"
       >
         <template slot-scope="props">
           <b-table-column
@@ -25,118 +25,145 @@
           <b-table-column
             :visible="columnsVisible['ek'].display"
             :label="columnsVisible['ek'].title"
-            :centered=true
+            :centered="true"
             width="500"
           >
+            5/25
           </b-table-column>
 
           <b-table-column
             :visible="columnsVisible['ak'].display"
             :label="columnsVisible['ak'].title"
             width="500"
-            :centered=true
+            :centered="true"
           >
           </b-table-column>
         </template>
 
         <template slot="detail" slot-scope="props">
-          <template v-for="item in props.row.criterium_results" >
-          <tr :key="item.name">
-            <td v-show="columnsVisible['name'].display">
-              <div
-                class="type-tag"
-                :class="{
-                  greenBackgroundClass: item.criterium_type == 'EK',
-                  redBackgroundClass: item.criterium_type == 'AK',
-                }"
-              >
-                {{ item.criterium_type }}
-              </div>
-              {{ item.name }}
-            </td>
+          <template v-for="item in props.row.criterium_results">
+            <tr :key="item.name">
+              <td v-show="columnsVisible['name'].display">
+                <div
+                  class="type-tag"
+                  :class="{
+                    greenBackgroundClass: item.criterium_type == 'EK',
+                    redBackgroundClass: item.criterium_type == 'AK',
+                  }"
+                >
+                  {{ item.criterium_type }}
+                </div>
+                {{ item.name }}
+              </td>
 
-            <template v-if="item.criterium_type == 'EK'">
-              <td
-                v-show="columnsVisible['ek'].display"
-                :class="{
-                  greenFillClass: item.criterium_summary_result == 0,
-                  
-                }"
-                class="has-text-centered"
-              >
-              <!-- redFillClass: item.criterium_type == 1, -->
-                ⬤
-              </td>
-              <td v-show="columnsVisible['ak'].display"></td>
-            </template>
+              <template v-if="item.criterium_type == 'EK'">
+                <td
+                  v-show="columnsVisible['ek'].display"
+                  :class="{
+                    greenFillClass: item.criterium_summary_result == 'hit',
+                    greyFillClass: item.criterium_summary_result == 'no_hit',
+                    redFillClass:
+                      item.criterium_summary_result == 'negative_hit',
+                  }"
+                  class="has-text-centered"
+                >
+                  ⬤
+                </td>
+                <td v-show="columnsVisible['ak'].display"></td>
+              </template>
 
-            <template v-else>
-              <td v-show="columnsVisible['ek'].display"></td>
-              <td
-                v-show="columnsVisible['ak'].display"
-                :class="{
-                  
-                  redFillClass: item.criterium_summary_result == 0,    
-                }"
-                class="has-text-centered"
-              >
-              <!-- greenFillClass: item.criterium_summary_result == 1, -->
-              
-                ⬤
+              <template v-else>
+                <td v-show="columnsVisible['ek'].display"></td>
+                <td
+                  v-show="columnsVisible['ak'].display"
+                  :class="{
+                    greenFillClass:
+                      item.criterium_summary_result == 'negative_hit',
+                    greyFillClass: item.criterium_summary_result == 'no_hit',
+                    redFillClass: item.criterium_summary_result == 'hit',
+                  }"
+                  class="has-text-centered"
+                >
+                  ⬤
+                </td>
+              </template>
+            </tr>
+            <tr v-for="condition in item.conditions" :key="condition.name">
+              <td class="condition-td" @click="openSource(condition, item)">
+                &emsp;&emsp;&emsp;{{ condition.name }}
               </td>
-            </template>
-          </tr>
-          <tr v-for="condition in item.conditions" :key="condition.name">
-            <td class="condition-td">
-             &emsp;&emsp;&emsp;{{ condition.name }}
-            </td>
-            <template v-if="item.criterium_type == 'EK'">
-              <td
-                v-show="columnsVisible['ek'].display"
-                :class="{
-                  greenFillClass: Array.isArray(condition.evaluation_result) && condition.evaluation_result.length > 0,
-                  redFillClass: condition.evaluation_result.length == 0,
-                }"
-                class="has-text-centered condition-details" 
-              >
-                ⬤
-              </td>
-              <td v-show="columnsVisible['ak'].display"></td>
-            </template>
+              <template v-if="item.criterium_type == 'EK'">
+                <td
+                  v-show="columnsVisible['ek'].display"
+                  :class="{
+                    greenFillClass:
+                      condition.evaluation_results.hits.length > 0,
+                    greyFillClass:
+                      condition.evaluation_results.hits.length == 0,
+                  }"
+                  class="has-text-centered condition-details"
+                >
+                  ⬤
+                </td>
+                <td v-show="columnsVisible['ak'].display"></td>
+              </template>
 
-            <template v-else>
-              <td v-show="columnsVisible['ek'].display"></td>
-              <td
-                v-show="columnsVisible['ak'].display"
-               :class="{
-                  greenFillClass: condition.evaluation_result.length == 0,
-                  redFillClass: Array.isArray(condition.evaluation_result) && condition.evaluation_result.length > 0,
-                }"
-                class="has-text-centered condition-details" 
-              >
-                ⬤
-              </td>
-            </template>
-          </tr>
+              <template v-else>
+                <td v-show="columnsVisible['ek'].display"></td>
+                <td
+                  v-show="columnsVisible['ak'].display"
+                  :class="{
+                    redFillClass: condition.evaluation_results.hits.length > 0,
+                    greyFillClass:
+                      condition.evaluation_results.hits.length == 0,
+                  }"
+                  class="has-text-centered condition-details"
+                >
+                  ⬤
+                </td>
+              </template>
+            </tr>
           </template>
         </template>
       </b-table>
+      <div class="popup-container">
+        <transition name="fade" appear>
+          <div
+            class="modal-overlay"
+            v-if="showModal"
+            @click="showModal = false"
+          ></div>
+        </transition>
+
+        <transition name="slide" appear>
+          <div class="popup-window" v-if="showModal">
+           <ConditionDetails :currentCondition="currentCondition" :currentCriterium="currentCriterium"/>
+          </div>
+        </transition>
+      </div>
     </div>
   </main>
 </template>
 
 <script>
+import ConditionDetails from "./ConditionDetails"
+
+
 export default {
   name: "ResultList",
+  components: {ConditionDetails},
   props: ["response"],
   data() {
     return {
+      showModal: false,
       columnsVisible: {
         name: { title: "Patientenname", display: true },
         conditionName: { title: "Bedingung", display: true },
-        ek: { title: "Einschlusskriterien", display: true },
+        ek: { title: "Einschlusskriterien", display: true }, 
         ak: { title: "Ausschlusskriterien", display: true },
       },
+     
+      currentCondition: {},
     };
   },
   created: function() {
@@ -146,6 +173,11 @@ export default {
     toggle(row) {
       this.$refs.table.toggleDetails(row);
     },
+    openSource(condition, item) {
+      this.showModal = true;
+      this.currentCondition = condition;
+      this.currentCriterium = item;
+    },
   },
 };
 </script>
@@ -153,13 +185,13 @@ export default {
 <style lang="scss">
 .type-tag {
   border-radius: 4px;
-  padding: 0 1vh;
+  // padding: 0 1vh;
   height: 50%;
   display: flex;
   justify-content: center;
   align-items: center;
   float: left;
-  margin-right: 2vh;
+  
   color: white;
 }
 .greenBackgroundClass {
@@ -178,8 +210,49 @@ export default {
   color: rgb(223, 51, 51);
 }
 
-.condition-details{
-  font-size: small;
+.greyFillClass {
+  color: rgb(154, 154, 154);
+}
 
+.condition-details {
+  font-size: small;
+}
+
+.popup-container {
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  // min-height: 100vh;
+  // overflow: hidden;
+  // background-color: salmon;
+}
+
+.modal-overlay {
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  z-index: 98;
+  background-color: rgba(250, 250, 250, 0.41);
+}
+.popup-window {
+  position: absolute;
+  top: 35%;
+  left: 70%;
+  transform: translate(-50%, -50%);
+  z-index: 99;
+  height: 50%;
+  width: 50%;
+  background-color: white;
+}
+.fade-enter-active,
+fade-leave-active {
+  transition: opacity 0.5s;
+}
+
+.fade-enter,
+fade-leave-to {
+  opacity: 0;
 }
 </style>
