176d949116aa694462e936a3ea35a1a365ba4a1f
Raik <raik.mueller@copra-system.de>
2021-01-20 14:43:24 +0100

    New Feature Export Function

--- clivio/src/renderer/pages/SelectedPatientsHistoryPage/index.vue
+++ clivio/src/renderer/pages/SelectedPatientsHistoryPage/index.vue
@@ -1,27 +1,41 @@
 <template>
   <main>
     <AppHeader :headerName="headerName" />
-   <div class="boxContainer">
+    <div class="boxContainer">
       <StudyBasicinfos :study="this.$route.query[1]" />
     </div>
 
-    <div class="boxContainer">
-    <ResultList
-      ref="ResultList"
-      :patientListDefault="patientListDefault"
-    />
- </div>
-    <b-button rounded id="btn-save-selected-patients" @click="saveSelectedPatients()"
+    <div
+      class="boxContainer"
+      v-if="
+        typeof patientListDefault !== 'undefined' &&
+        patientListDefault.length > 0
+      "
+    >
+      <ResultList ref="ResultList" :patientListDefault="patientListDefault" />
+    </div>
+    <div class="boxContainer" v-else>
+      Keine PatientInnen wurden für diese Studie bisher ausgewählt!
+    </div>
+    <b-button
+      rounded
+      id="btn-save-selected-patients"
+      @click="saveSelectedPatients()"
       >Speichern und Beenden</b-button
     >
-   
+    <b-button
+      rounded
+      id="btn-export-selected-patients"
+      @click="exportSelectedPatients()"
+      >Export</b-button
+    >
   </main>
 </template>
 
 <script>
 import AppHeader from "../../components/AppHeader";
 import ResultList from "../EvaluationPage/ResultList";
-import StudyBasicinfos from "../StudyPage/StudyBasicinfos"
+import StudyBasicinfos from "../StudyPage/StudyBasicinfos";
 
 export default {
   name: "SelectedPatientsHistoryPage",
@@ -42,7 +56,7 @@ export default {
       const rejectedData = this.$refs.ResultList.rejectedPatients;
       var rejectedDataJson = JSON.stringify(rejectedData);
 
-      formData.append("Study_Id",  this.$route.query[1].id);
+      formData.append("Study_Id", this.$route.query[1].id);
       formData.append("Rejected_Patients[]", rejectedDataJson);
 
       axios({
@@ -52,21 +66,42 @@ export default {
         headers: { "Content-Type": "multipart/form-data" },
       })
         .then((response) => {
-         this.showToastInfo("Auswahl gespeichert");
+          this.showToastInfo("Auswahl gespeichert");
         })
         .catch((response) => {
           this.showToastInfo(response.data);
         });
       this.$router.push({ name: "dashboard-page" });
     },
+    exportSelectedPatients() {
+      this.showToastInfo("export");
+      //create file
+      if (
+        typeof this.patientListDefault !== "undefined" &&
+        this.patientListDefault.length > 0
+      ) {
+        var dictstring = JSON.stringify(this.patientListDefault);
 
+        const os = require("os");
+        const strDesktopPath = os.homedir() + "/Desktop"
+
+        var fs = require("fs");
+        fs.writeFile(strDesktopPath + "/export.json", dictstring);
+      } else {
+        this.showToastError("Liste ist leer");
+      }
+
+      //write json into it
+
+      //
+    },
   },
-
 };
 </script>
 
 <style lang="scss">
-#btn-save-selected-patients {
+#btn-save-selected-patients,
+#btn-export-selected-patients {
   margin-top: 25px;
   margin-right: 25px;
   float: right;
